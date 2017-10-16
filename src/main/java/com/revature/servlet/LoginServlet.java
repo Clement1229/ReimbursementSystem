846a8f5f01cb39c0a497f6f4c55ead9f7b01f77f
Clement Lin <x4383lin@gmail.com>
2017-10-15 21:42:30 -0400

    jsp changed to html

--- src/main/java/com/revature/servlet/LoginServlet.java
+++ src/main/java/com/revature/servlet/LoginServlet.java
@@ -20,14 +20,8 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#HttpServlet()
 	 */
 	public LoginServlet() {
-		super();
-		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		System.out.println("LoginServlet -GET");
@@ -36,10 +30,6 @@ public class LoginServlet extends HttpServlet {
 
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		System.out.println("LoginServlet -POST");
@@ -49,9 +39,11 @@ public class LoginServlet extends HttpServlet {
 		int rtid = service.getRtidByUsernamePassword(username, password);
 		System.out.println("role type: " + rtid);
 		if (rtid == 1)
-			request.getRequestDispatcher("manager_home.jsp").forward(request, response);
-		if (rtid == 2)
-			request.getRequestDispatcher("employee_home.jsp").forward(request, response);
+			request.getRequestDispatcher("manager/mng_home.html").forward(request, response);
+		else if (rtid == 2)
+			request.getRequestDispatcher("employee/emp_home.html").forward(request, response);
+		else
+			response.sendRedirect("login.html");
 	}
 
 }
