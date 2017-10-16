c92cccde21d5b0fd79f4f5f074669ceab6a28bc4
Clement Lin <x4383lin@gmail.com>
2017-10-15 23:59:47 -0400

    Navbar employee ->navbar part

--- src/main/java/com/revature/servlet/LoginServlet.java
+++ src/main/java/com/revature/servlet/LoginServlet.java
@@ -2,6 +2,7 @@ package com.revature.servlet;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,7 @@ import com.revature.service.Service;
 /**
  * Servlet implementation class Login
  */
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
